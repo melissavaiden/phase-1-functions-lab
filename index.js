d6e64dcdb7616db079391355a8086e3fe18aa0f5
Melissa Vaiden <melissavaiden@gmail.com>
2022-01-18 21:15:45 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Code your solution in this file!
+let hqLocation = 42;
+let cityBlockInFeet = 264
+
+function distanceFromHqInBlocks(currentLocation) {
+    if (currentLocation >= hqLocation) {
+        return currentLocation - hqLocation;
+    } else {
+        return hqLocation - currentLocation;
+    }
+}
+function distanceFromHqInFeet(currentLocation) {
+    const distance = distanceFromHqInBlocks(currentLocation);
+    let distanceInFeet = distance * cityBlockInFeet;
+    return distanceInFeet;
+}
+function distanceTravelledInFeet(start, destination) {
+    if (start > destination) {
+        return (start - destination) * cityBlockInFeet;
+    } else {
+        return (destination - start) * cityBlockInFeet;
+    }
+}
+function calculatesFarePrice(start, destination) {
+    const fareDistance = distanceTravelledInFeet (start, destination);
+    if (fareDistance < 400) {
+        return 0;
+    } else if (fareDistance > 400 && fareDistance <= 2000) {
+        return 0.02 * (fareDistance - 400);
+    } else if (fareDistance > 2000 && fareDistance <  2500) {
+        return 25;
+    } else return "cannot travel that far";
+}
+
